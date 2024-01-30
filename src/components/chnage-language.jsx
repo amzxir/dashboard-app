c9f92de7a0069ad7055d79361c2536580d595eb9
amzxir <amirreza79ahmadi@gmail.com>
2024-01-30 16:04:41 +0330

    multip language

--- src/components/chnage-language.jsx
+++ src/components/chnage-language.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import enFlag from "@assets/images/us.png";
 import faFlag from "@assets/images/fa.png";
+import { useAppContext } from "../context/app/app-context";
 
 const ChangeLanguage = () => {
-    const [isOpen , setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const { chnageLan , language } = useAppContext();
 
     const ref = useRef();
 
@@ -14,25 +17,25 @@ const ChangeLanguage = () => {
                 setIsOpen(false);
             }
         }
-        
-        document.addEventListener('mousedown' , checkIfCleckOutSide)
+
+        document.addEventListener('mousedown', checkIfCleckOutSide)
 
         return () => {
-            document.removeEventListener('mousedown' , checkIfCleckOutSide)
+            document.removeEventListener('mousedown', checkIfCleckOutSide)
         }
-    },[isOpen])
+    }, [isOpen])
 
-    return(
+    return (
         <div className="dropdown">
             <a onClick={() => setIsOpen(true)} className="nav-flag dropdown-toggle">
-                <img src={enFlag} alt="" />
+                <img src={language === "fa" ? faFlag : enFlag} alt="" />
             </a>
             <div ref={ref} className={`dropdown-menu dropdown-menu-end ${isOpen && 'show'}`}>
-                <a className="dropdown-item fw-bolder text-end">
+                <a className={`dropdown-item fw-bolder d-flex align-items-center gap-2 ${language === 'fa' ? 'text-end' : 'text-start'}`} onClick={() => chnageLan("fa")}>
                     <img src={faFlag} width="20" className="ms-2" alt="" />
                     <span className="align-middle">فارسی</span>
                 </a>
-                <a className="dropdown-item fw-bolder text-end">
+                <a className={`dropdown-item fw-bolder d-flex align-items-center gap-2 ${language === 'fa' ? 'text-end' : 'text-start'}`} onClick={() => chnageLan("en")}>
                     <img src={enFlag} width="20" className="ms-2" alt="" />
                     <span className="align-middle">English</span>
                 </a>
