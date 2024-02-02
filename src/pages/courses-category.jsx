0b52e13616976c20428b7dc9b1ba74745a6eb282
amzxir <amirreza79ahmadi@gmail.com>
2024-02-02 14:45:21 +0330

    inviment varibale

--- src/pages/courses-category.jsx
+++ src/pages/courses-category.jsx
@@ -35,7 +35,7 @@ export async function coursesCategoryLoader({ request }) {
 
 const loaderCategory = async (request) => {
     const page = new URL(request.url).searchParams.get('page') || 1;
-    const pageSize = 6;
+    const pageSize = import.meta.env.VITE_PAGE_SIZE;
     let url = '/CourseCategory/sieve';
     url += `?page=${page}&pageSize=${pageSize}`
     const response = await HttpInterCeptoredService.get(url);
